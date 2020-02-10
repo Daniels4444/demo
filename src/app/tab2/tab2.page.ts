926005e496337534580fec33cc097deda00474ac
Kriss D. D. BASSIDI B <blacklenoire4444@gmail.com>
2020-02-10 11:39:00 +0100

    commit 001

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,90 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+	
+	private temp : number;
+	private signe : number;
+	private vider : number;
+	constructor()
+	{
+		this.resultat = 0;
+		this.temp = 0;
+		this.signe = 0;
+		this.reset = 0;
+	}
 
-  constructor() {}
+	effacer()
+	{
+		this.resultat = 0;
+		this.temp = 0;
+		this.signe = 0;
+		this.vider = 0;
+	}
+	
+	signe()
+	{
+		
+	}
+	
+	operation(operateur : int)
+	{
+		this.temp = this.resultat;
+		this.signe = operateur;
+		this.vider = 1;
+	}
 
+	afficherNombre(nombre : number)
+	{
+		if(this.vider == 1)
+		{
+			this.resultat = 0;
+			this.vider = 0;
+		}
+		else if(this.vider == 2)
+		{
+			this.resultat = 0;
+			this.temp = 0;
+			this.signe = 0;
+			this.vider = 0;
+		}
+		if(this.resultat == 0)
+		{
+			this.resultat = nombre;
+		}
+		else
+		{
+			
+			this.resultat = this.resultat + nombre;
+		}
+	}
+	
+	virgule()
+	{
+		
+	}
+	
+	calculer()
+	{
+		if(this.signe == 1)
+		{
+			this.resultat = this.temp % this.resultat;
+		}
+		else if(this.signe == 2)
+		{
+			this.resultat = this.temp / this.resultat;
+		}
+		else if(this.signe == 3)
+		{
+			this.resultat = this.temp * this.resultat;
+		}
+		else if(this.signe == 4)
+		{
+			this.resultat = this.temp - this.resultat;
+		}
+		else if(this.signe == 5)
+		{
+			this.resultat = this.temp + this.resultat;
+		}
+		this.vider = 2;
+	}
 }
